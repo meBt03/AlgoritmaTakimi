7f6bc0ff188d87931de4709005d57a3254c8ea1d
meBt03 <88210866+meBt03@users.noreply.github.com>
2025-03-11 15:40:22 +0300

    Create Buble Sort Algorithm.py

---
+++ 3.Hafta/Buble Sort Algorithm/Buble Sort Algorithm.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  
+                swapped = True
+        if not swapped:
+            break  
+    return arr
+
